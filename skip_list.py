ee6a2dac3262912eb2bfc66972f9f009018414dd
kcottur <kcottur@uci.edu>
2024-03-07 17:39:52 -0800

    skip list

--- skip_list.py
+++ skip_list.py
@@ -1,42 +1,90 @@
-# explanations for member functions are provided in requirements.py
-# each file that uses a skip list should import it from this file.
-
-from typing import TypeVar
+from typing import TypeVar, Optional, List, Tuple
 import random
 from zip_tree import ZipTree
 
 KeyType = TypeVar('KeyType')
 ValType = TypeVar('ValType')
 
+class SkipListNode:
+    def __init__(self, key: KeyType, val: ValType, level: int):
+        self.key = key
+        self.val = val
+        # Array to hold references to node of different levels
+        self.forward = [None] * (level + 1)
+
 class SkipList:
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.max_level = 20  # Maximum level for this skip list
+        self.head = SkipListNode(None, None, self.max_level)  # header node
+        self.level = 0  # Current level of skip list
 
-	def get_random_level(self, key: KeyType) -> int:
-	  	# Do not change this function. Use this function to determine what level each key should be at. Assume levels start at 0 (i.e. the bottom-most list is at level 0)
-		# e.g. for some key x, if get_random_level(x) = 5, then x should be in the lists on levels 0, 1, 2, 3, 4 and 5 in the skip list.
-		random.seed(str(key))
-		level = 0
-		while random.random() < 0.5 and level < 20:
-			level += 1
-		return level
+    def get_random_level(self, key: KeyType) -> int:
+        random.seed(str(key))
+        level = 0
+        while random.random() < 0.5 and level < self.max_level:
+            level += 1
+        return level
 
-	def insert(self, key: KeyType, val: ValType):
-		pass
+    def insert(self, key: KeyType, val: ValType):
+        update = [None] * (self.max_level + 1)
+        current = self.head
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].key < key:
+                current = current.forward[i]
+            update[i] = current
 
-	def remove(self, key: KeyType):
-		pass
+        current = current.forward[0]
 
-	def find(self, key: KeyType) -> ValType:
-		pass
+        if current is None or current.key != key:
+            new_level = self.get_random_level(key)
+            if new_level > self.level:
+                for i in range(self.level + 1, new_level + 1):
+                    update[i] = self.head
+                self.level = new_level
 
-	def get_list_size_at_level(self, level: int):
-		pass
-		
-	def from_zip_tree(self, zip_tree: ZipTree) -> None:
-		pass
-			
+            new_node = SkipListNode(key, val, new_level)
+            for i in range(new_level + 1):
+                new_node.forward[i] = update[i].forward[i]
+                update[i].forward[i] = new_node
 
-# feel free to define new classes/methods in addition to the above
-# fill in the definitions of each required member function (above),
-# and any additional member functions you define
+    def remove(self, key: KeyType):
+        update = [None] * (self.level + 1)
+        current = self.head
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].key < key:
+                current = current.forward[i]
+            update[i] = current
+
+        current = current.forward[0]
+        if current and current.key == key:
+            for i in range(self.level + 1):
+                if update[i].forward[i] != current:
+                    break
+                update[i].forward[i] = current.forward[i]
+
+            while self.level > 0 and self.head.forward[self.level] is None:
+                self.level -= 1
+
+    def find(self, key: KeyType) -> Optional[ValType]:
+        current = self.head
+        for i in range(self.level, -1, -1):
+            while current.forward[i] and current.forward[i].key < key:
+                current = current.forward[i]
+        current = current.forward[0]
+        if current and current.key == key:
+            return current.val
+        return None
+
+    def get_list_size_at_level(self, level: int) -> int:
+        if level > self.level:
+            return 0
+        size = 0
+        node = self.head.forward[level]
+        while node:
+            size += 1
+            node = node.forward[level]
+        return size
+
+    def from_zip_tree(self, zip_tree: ZipTree) -> None:
+        # Implementation depends on traversal of the ZipTree and using the insert method here
+        pass
